120cc4e93819a01740c26317bf7aa753727314f2
Chad Palmer <chad.n.palmer@gmail.com>
2021-02-10 21:13:58 -0600

    Solved factorial, sum, arraySum, isEven

--- src/recursion.js
+++ src/recursion.js
@@ -7,20 +7,51 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if (n < 0) { 
+    return null; 
+  } else if (n === 0) { 
+    return 1; 
+  } else { 
+    return (n * factorial(n - 1)); 
+  }
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  } else if (array.length === 1) {
+    return array[0];
+  } else {
+    return array[0] + sum(array.slice(1));
+  }
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  // If array is empty, return 0
+  if (array.length === 0) {
+    return 0;
+  } else {
+    if (Array.isArray(array[0])) {
+      return arraySum(array[0]) + arraySum(array.slice(1));
+    } else {
+      return array[0] + arraySum(array.slice(1));
+    }
+  }
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  if (n === 0) {
+    return true;
+  } else if (Math.abs(n) - 2 < 0) {
+    return false;
+  } else {
+    return isEven(Math.abs(n) - 2);
+  }
 };
 
 // 5. Sum all integers below a given integer.
