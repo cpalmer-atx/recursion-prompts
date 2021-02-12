4e223253fb2128a26394f7146016454329a30eb8
Chad Palmer <chad.n.palmer@gmail.com>
2021-02-11 22:08:02 -0600

    incomplete attempts added, will need to refactor

--- src/recursion.js
+++ src/recursion.js
@@ -86,7 +86,7 @@ var exponent = function(base, exp) {
   } else if (exp > 0) {
     return base * exponent(base, (exp - 1));
   } else {
-    return Number.parseFloat(((1 / base) * exponent(base, (exp + 1)))).toFixed(4);
+    return Number.parseFloat(((1 / base) * exponent(base, (exp + 1))).toFixed(5));
   }
 };
 
@@ -103,6 +103,14 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  
+  if (string.length === 1) {
+    return true;
+  } else if (string[0] === string[string.length - 1]) {
+    return palindrome(string.slice(1, string.length - 1));
+  } else {
+    return false;
+  }
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
